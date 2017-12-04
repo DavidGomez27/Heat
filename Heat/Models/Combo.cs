c4a8331e0d062cf034da167d7d243de92e4655ae
FaizeChan <obey.faize@gmail.com>
2017-12-04 09:04:46 -0500

    Displaying Names

--- Heat/Models/Combo.cs
+++ Heat/Models/Combo.cs
@@ -11,7 +11,8 @@ namespace Heat.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Combo
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,6 +22,7 @@ namespace Heat.Models
         }
     
         public int CouplesComboID { get; set; }
+        [Display(Name = "Combo")]
         public string Combo1 { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
