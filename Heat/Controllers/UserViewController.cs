718fb1b6b848c0fc204f5cd37cbedab9afdde79a
DStanarevic <DejanStanarevic@gmail.com>
2017-12-05 13:40:25 -0500

    test

---
+++ Heat/Controllers/UserViewController.cs
@@ -0,0 +1,26 @@
+﻿using Heat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace Heat.Controllers
+{
+    public class UserViewController : Controller
+    {
+        private HeatEntities db = new HeatEntities();
+        // GET: UserView
+        public ActionResult Index()
+        {
+            var couples = db.Couples.Include(c => c.Combo).Include(c => c.DanceLevel).Include(c => c.DanceType).Include(c => c.HeatList).Include(c => c.Pro);
+            
+            //var heatName = (from Couple in db.Couples
+            //                where Couple.HeatList.Name.Contains(HeatList.Name)
+            //                select Couple).ToList();
+          
+            return View(couples.ToList());
+        }
+    }
+}
