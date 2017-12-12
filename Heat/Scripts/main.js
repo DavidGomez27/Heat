b2e8967e2a399c5a00d88589483b71feb16577d0
FaizeChan <obey.faize@gmail.com>
2017-12-12 17:34:10 -0500

    trying to combine style with code

--- Heat/Scripts/main.js
+++ Heat/Scripts/main.js
@@ -77,10 +77,10 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{ image: 'img/slider-images/image01.jpg', title: '<div class="slide-content">Turn</div>', thumb: '', url: '' },
-											{ image: 'img/slider-images/image02.jpg', title: '<div class="slide-content">Up</div>', thumb: '', url: '' },
-											{ image: 'img/slider-images/image03.jpg', title: '<div class="slide-content">the</div>', thumb: '', url: '' },
-											{ image: 'img/slider-images/image04.jpg', title: '<div class="slide-content">HEAT</div>', thumb: '', url: '' }
+											{ image: '../Content/img/slider-images/image01.jpg', title: '<div class="slide-content">Turn</div>', thumb: '', url: '' },
+											{ image: '../Content/img/slider-images/image02.jpg', title: '<div class="slide-content">Up</div>', thumb: '', url: '' },
+											{ image: '../Content/img/slider-images/image03.jpg', title: '<div class="slide-content">the</div>', thumb: '', url: '' },
+											{ image: '../Content/img/slider-images/image04.jpg', title: '<div class="slide-content">HEAT</div>', thumb: '', url: '' }
 									],
 									
 		// Theme Options			   
